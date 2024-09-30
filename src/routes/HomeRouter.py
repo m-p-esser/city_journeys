cd22ee85a559113f631b401e2d2e6998417c2d49
Marc-Philipp Esser <m-esser@mail.de>
2024-09-30 21:07:00 +0200

    rename folder and switch from php to python

---
+++ src/routes/HomeRouter.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+from src.controller.home_controller import HomeController
+
+home_router = Blueprint(name="home_controller", import_name=__name__)
+home_router.route("/", methods=["GET"])(HomeController.render)
