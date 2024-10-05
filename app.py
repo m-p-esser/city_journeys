ffb39f5c4cbf1d4b08262fe49a77022e36fda0ce
Marc-Philipp Esser <m-esser@mail.de>
2024-10-05 14:55:31 +0200

    reorderand import database model (so they can be created trough alembic migrations

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from dotenv import find_dotenv, load_dotenv
+from dotenv import load_dotenv
 from src.db import DatabaseClient
-from router import Router
+from src.router import Router
 
 template_dir = pathlib.Path.cwd() / "public"
 static_dir = pathlib.Path.cwd() / "assets"
@@ -17,26 +17,31 @@ env_vars = os.environ
 app = Flask(
     import_name=__name__,
     template_folder=template_dir,
-    # static_folder=static_dir
+    static_folder=static_dir
 )
 
 # Init Database
-db = DatabaseClient(
+db_client = DatabaseClient(
     host=env_vars["HOST_NAME"],
     port=env_vars["PORT"],
     database=env_vars["DATABASE"],
     user=env_vars["USER_NAME"],
     password=env_vars["PASSWORD"],
 )
-connection_uri = db.create_connection_uri("MySQL")
-app.config.from_mapping(
-    {"SQLALCHEMY_DATABASE_URI": connection_uri}
-)
+db_client.create_database(db_client.database)
+
+connection_uri = db_client.create_connection_uri("MySQL")
+app.config.from_mapping({"SQLALCHEMY_DATABASE_URI": connection_uri})
+db = SQLAlchemy(app)
 
 # Migrate Database
 migrate = Migrate(app, db)
 
+# Start App on Landing page
 Router.run(app)
+
+from src import models
+
 
 if __name__ == "__main__":
     app.debug = True
