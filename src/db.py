bc31285ff27bc0261c571b5dbc912602bfdf504e
Marc-Philipp Esser <m-esser@mail.de>
2024-10-05 14:51:28 +0200

    allow connecting trough mysqlconnector driver and add function to create database

--- src/db.py
+++ src/db.py
@@ -25,20 +25,29 @@ class DatabaseClient:
                 f"Not allowed. Only the following dialects are allowed: {self.allowed_dialects}"
             )
 
-        if dialect in ["MySQL", "PostgreSQL"]:
-            connection_uri = (
-                f"{dialect}://{self.user}:{self.password}@{self.host}/{self.database}"
-            )
+        if dialect in ["MySQL"]:
+
+            dialect_driver_mapping = {"MySQL": "mysql+mysqlconnector"}
+            dialect_driver = dialect_driver_mapping[dialect]
+
+            connection_uri = f"{dialect_driver}://{self.user}:{self.password}@{self.host}/{self.database}"
             return connection_uri
 
         if dialect == "SQLite":
             raise NotImplementedError
 
     def open_connection(self):
-        self.conn = connect(user=self.user, database=self.database)
+        self.conn = connect(
+            user=self.user, password=self.password, host=self.host, database="mysql"
+        )
 
     def close_connection(self):
         self.conn.close()
+
+    def create_database(self, database):
+        self.open_connection()
+        self.run_query(f"CREATE DATABASE IF NOT EXISTS {database}")
+        self.close_connection()
 
     def run_query(self, query_string):
         result = self.conn.cmd_query(query_string)
