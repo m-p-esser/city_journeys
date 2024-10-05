fa9b2ae5a57075e8c9e52c6713197a9b20f2cbdc
Marc-Philipp Esser <m-esser@mail.de>
2024-10-05 14:52:29 +0200

    add cities table as imperative sqlalchemy model

--- src/models.py
+++ src/models.py
@@ -2,9 +2,19 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 
+# metadata = sa.MetaData()
 
-class City(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+city = db.Table(
+    'cities',
+    # metadata,
+    sa.Column('id', sa.BigInteger, primary_key=True, nullable=False),
+    sa.Column('name', sa.String(255), nullable=False),
+    sa.Column('state_id', sa.BigInteger, nullable=False),
+    sa.Column('country_id', sa.BigInteger, nullable=False),
+    sa.Column('latitude', sa.Numeric(10,8), nullable=False),
+    sa.Column('longitude', sa.Numeric(11,8), nullable=False),
+    sa.Column('wikiDataId', sa.String(255)),
+    sa.Column('population', sa.BigInteger),
+    sa.Column('elevation_in_meter', sa.BigInteger)
+)
 
-    def __repr__(self):
-        return "<User {}>".format(self.username)
