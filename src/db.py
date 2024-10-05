616345a97c3aadd558f4dbf55a1d637a15ec7071
Marc-Philipp Esser <m-esser@mail.de>
2024-10-05 20:22:44 +0200

    use db config in database client methods2

--- src/db.py
+++ src/db.py
@@ -1,23 +1,23 @@
 from mysql.connector import connect
 from typing import Literal
+import pandas as pd
+import csv
+from config.config import db_config
+from src.utils import find_root, recursively_find_file
 
 
 class DatabaseClient:
 
     allowed_dialects = ["MySQL", "PostgreSQL", "SQLite"]
 
-    def __init__(
-        self, host: str, port: str, database: str, user: str, password: str
-    ) -> None:
-        self.host = host
-        self.port = port
-        self.database = database
-        self.user = user
-        self.password = password
-        self.connection = None
+    def __init__(self) -> None:
+        self.conn = None
+        self.config = db_config
 
-    def create_connection_uri(self, dialect: Literal["MySQL", "PostgreSQL", "SQLite"]):
-
+    def create_connection_uri(
+        self,
+        dialect: Literal["MySQL", "PostgreSQL", "SQLite"],
+    ):
         connection_uri = None
 
         if dialect not in self.allowed_dialects:
@@ -30,7 +30,7 @@ class DatabaseClient:
             dialect_driver_mapping = {"MySQL": "mysql+mysqlconnector"}
             dialect_driver = dialect_driver_mapping[dialect]
 
-            connection_uri = f"{dialect_driver}://{self.user}:{self.password}@{self.host}/{self.database}"
+            connection_uri = f"{dialect_driver}://{db_config.DB_USER_NAME}:{db_config.DB_USER_PASSWORD}@{db_config.DB_HOST_IP}/{db_config.DB_NAME}"
             return connection_uri
 
         if dialect == "SQLite":
@@ -38,7 +38,10 @@ class DatabaseClient:
 
     def open_connection(self):
         self.conn = connect(
-            user=self.user, password=self.password, host=self.host, database="mysql"
+            user=db_config.DB_USER_NAME,
+            password=db_config.DB_USER_PASSWORD,
+            host=db_config.DB_HOST_IP,
+            database="mysql",
         )
 
     def close_connection(self):
@@ -50,5 +53,36 @@ class DatabaseClient:
         self.close_connection()
 
     def run_query(self, query_string):
+        self.open_connection()
         result = self.conn.cmd_query(query_string)
+        self.close_connection()
         return result
+
+    def insert_from_csv(self, file_name, table_name, database):
+        file_type = file_name.split(".")[-1]
+
+        if file_type != "csv":
+            raise NotImplementedError
+
+        root_dir = find_root()
+        csv_file_path = recursively_find_file(root_dir, file_name)
+
+        self.open_connection()
+
+        cur = self.conn.cursor()
+        with open(csv_file_path) as fp:
+            reader = csv.reader(fp, delimiter=",")
+            for idx, row in enumerate(reader):
+                if idx == 0:
+                    header_columns = row
+                    value_placeholders = ",".join(['%s']*len(header_columns))
+                    # print(value_placeholders)
+                    header_columns = ",".join(header_columns)
+                if idx > 0 & idx < 10:
+                    # row = ",".join(f"'{c}'" for c in row)
+                    sql = f"INSERT INTO {database}.{table_name} ({header_columns}) VALUES ({value_placeholders})"
+                    cur.execute(sql, row)
+                    # self.run_query(sql)
+        cur.close()
+
+        self.close_connection()
