ebb1c8c18a9f4e13ba176388a586fbcfd98e16ac
Marc-Philipp Esser <m-esser@mail.de>
2024-10-05 20:23:36 +0200

    create db client using db config and move env loading into config module

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 import pathlib
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from dotenv import load_dotenv
 from src.db import DatabaseClient
 from src.router import Router
 
 template_dir = pathlib.Path.cwd() / "public"
 static_dir = pathlib.Path.cwd() / "assets"
-
-load_dotenv()
-env_vars = os.environ
 
 # Start the app
 app = Flask(
@@ -21,14 +16,8 @@ app = Flask(
 )
 
 # Init Database
-db_client = DatabaseClient(
-    host=env_vars["HOST_NAME"],
-    port=env_vars["PORT"],
-    database=env_vars["DATABASE"],
-    user=env_vars["USER_NAME"],
-    password=env_vars["PASSWORD"],
-)
-db_client.create_database(db_client.database)
+db_client = DatabaseClient()
+db_client.create_database(db_client.config.DB_NAME)
 
 connection_uri = db_client.create_connection_uri("MySQL")
 app.config.from_mapping({"SQLALCHEMY_DATABASE_URI": connection_uri})
