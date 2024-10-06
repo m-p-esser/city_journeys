d86333847e0a8912556db75869f16a7c7c9d16f4
Marc-Philipp Esser <m-esser@mail.de>
2024-10-06 12:04:43 +0200

    rename template render method and add logic to query city and country data from database

--- src/controller/home_controller.py
+++ src/controller/home_controller.py
@@ -1,6 +1,31 @@
 from flask import render_template
+from config.config import static_file_space_config, db_config
+from src.db import DatabaseClient
+
 
 class HomeController:
 
-    def render():
-        return render_template('index.html')
+    # def construct_cnd_url(image_path):
+    #     endpoint_url = static_file_space_config.SPACES_ENDPOINT_URL
+    #     region_name = static_file_space_config.SPACES_REGION_NAME
+    #     aws_access_key_id = static_file_space_config.SPACES_AWS_ACCESS_KEY_ID
+    #     aws_secret_access_key = static_file_space_config.SPACES_AWS_SECRET_ACCESS_KEY
+
+    #     cdn_domain = f"https://travel-app-prod-images.{region_name}.cdn.digitaloceanspaces.com"
+    #     return f"{cdn_domain}/{image_path}"
+
+    def view_cities():
+        db_client = DatabaseClient()
+        sql = """
+        SELECT 
+            cities.id,
+            cities.name AS city_name,
+            countries.name AS country_name,
+            countries.emoji AS country_emoji 
+        FROM db_prod.cities AS cities
+        LEFT JOIN db_prod.countries AS countries
+        ON cities.country_id = countries.id
+        LIMIT 50
+        """
+        cities = db_client.read_query(sql)
+        return render_template("index.html", cities=cities)
--- src/routes/HomeRouter.py
+++ src/routes/HomeRouter.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 from src.controller.home_controller import HomeController
 
 home_router = Blueprint(name="home_controller", import_name=__name__)
-home_router.route("/", methods=["GET"])(HomeController.render)
+home_router.route("/", methods=["GET"])(HomeController.view_cities)
