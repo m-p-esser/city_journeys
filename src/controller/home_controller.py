2ba18f29561a1bdd08262554cd9d1b1424f1edbe
Marc-Philipp Esser <m-esser@mail.de>
2024-10-06 19:13:37 +0200

    move boto client inside cities view and use two requests to get city and country data required for city gallery

--- src/controller/home_controller.py
+++ src/controller/home_controller.py
@@ -1,23 +1,11 @@
 from flask import render_template
 import boto3
+import requests
 from config.config import static_file_space_config, db_config
 from src.db import DatabaseClient
 
 
-def construct_cnd_url(city_name: str, country_name: str):
-    endpoint_url = static_file_space_config.SPACES_ENDPOINT_URL
-    region_name = static_file_space_config.SPACES_REGION_NAME
-    aws_access_key_id = static_file_space_config.SPACES_AWS_ACCESS_KEY_ID
-    aws_secret_access_key = static_file_space_config.SPACES_AWS_SECRET_ACCESS_KEY
-
-    session = boto3.session.Session()
-    client = session.client(
-        "s3",
-        region_name=region_name,
-        endpoint_url=endpoint_url,
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key,
-    )
+def construct_cnd_url(client, city_name: str, country_name: str, region_name: str):
 
     response = client.list_objects(
         Bucket="travel-app-prod-images", Prefix=f"{city_name}_{country_name}"
@@ -35,23 +23,43 @@ def construct_cnd_url(city_name: str, country_name: str):
 class HomeController:
 
     def view_cities():
-        db_client = DatabaseClient()
-        sql = """
-        SELECT 
-            cities.id,
-            cities.name AS city_name,
-            cities.population AS city_population,
-            countries.name AS country_name,
-            countries.emoji AS country_emoji 
-        FROM db_prod.cities AS cities
-        LEFT JOIN db_prod.countries AS countries
-        ON cities.country_id = countries.id
-        WHERE cities.population > 100000
-        ORDER BY cities.population DESC
-        LIMIT 20
-        """
-        cities = db_client.read_query(sql)
-        for c in cities:
-            c["cdn_url"] = construct_cnd_url(c["city_name"], c["country_name"])
+        endpoint_url = static_file_space_config.SPACES_ENDPOINT_URL
+        region_name = static_file_space_config.SPACES_REGION_NAME
+        aws_access_key_id = static_file_space_config.SPACES_AWS_ACCESS_KEY_ID
+        aws_secret_access_key = static_file_space_config.SPACES_AWS_SECRET_ACCESS_KEY
 
-        return render_template("index.html", cities=cities)
+        session = boto3.session.Session()
+        client = session.client(
+            "s3",
+            region_name=region_name,
+            endpoint_url=endpoint_url,
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+        )
+
+        response = requests.get(url="http://127.0.0.1:8000/cities")
+        response.raise_for_status()
+        cities = response.json()
+
+        response = requests.get(url="http://127.0.0.1:8000/countries")
+        response.raise_for_status()
+        countries = response.json()
+
+        cities_expanded = []
+
+        for ct in cities:
+            country_id = ct["country_id"]
+            country = [cn for cn in countries if cn["id"] == country_id][0]
+            country_name, country_emoji = country["name"], country["emoji"]
+            ct["country_name"] = country_name
+            ct["country_emoji"] = country_emoji
+            ct["city_name"] = ct.pop("name")
+            ct["cdn_url"] = construct_cnd_url(
+                client=client,
+                city_name=ct["city_name"],
+                country_name=ct["country_name"],
+                region_name=region_name
+            )  # This step takes forever
+            cities_expanded.append(ct)
+
+        return render_template("index.html", cities=cities_expanded)
